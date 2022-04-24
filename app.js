232c244e1310b6b5c5d97a2394c8a8f80ee00ee8
timeabalogh <73299540+timeabalogh@users.noreply.github.com>
2022-04-24 14:38:37 +0200

    added function to have Xs and Os appear on the board and track them

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
+const board = document.querySelectorAll('.field');
+const playerX = 'X';
+const playerO = 'O';
+let turn = playerX;
+
+const boardState = Array(board.length);
+boardState.fill(null);
+
+const gameOverText = document.getElementById('victor');
+const resetBoard = document.getElementById('reset');
+
+//Sounds
+
+const clickSound = new Audio('audio/click.wav')
+
+// document.getElementById('next').innerText = turn;
+
+board.forEach(field => field.addEventListener('click', fieldClick));
+
+function fieldClick(event) {
+    if (gameOverText.classList.contains('visible')) {
+        return;
+    }
+
+    const field = event.target;
+    const fieldNumber = field.dataset.index;
+    if (field.innerText != '') {
+        return;
+    }
+
+    if (turn === playerX) {
+        field.innerText = playerX;
+        boardState[fieldNumber - 1] = playerX;
+        turn = playerO;
+    }
+
+    else {
+        field.innerText = playerO;
+        boardState[fieldNumber - 1] = playerO;
+        turn = playerX;
+    }
+
+    clickSound.play();
+};
+
+
+
 function clearUi() {
-    Array.from(document.getElementsByClassName('field')).forEach(function (element) {
+    Array.from(board).forEach(function (element) {
         element.innerText = "";
     })
-}
+};
 
-
-document.getElementById('reset').addEventListener('click', clearUi)
+resetBoard.addEventListener('click', clearUi);
